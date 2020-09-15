1378f432941713f3179b72f01acf4b967744d877
Crowdbotics <team@crowdbotics.com>
2020-09-15 18:00:04 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/revo_ex_20293/MainActivity.java
@@ -0,0 +1,15 @@
+package com.revo_ex_20293;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "revo_ex_20293";
+    }
+}
---
+++ ios/revo_ex_20293Tests/revo_ex_20293Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface revo_ex_20293 : XCTestCase
+
+@end
+
+@implementation revo_ex_20293
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
